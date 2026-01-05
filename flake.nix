6f007e78d09d829da6d3cc3ebffd18d64d09cdd2
David Ochsner <davidochsner93@gmail.com>
2026-01-05 12:26:46 +0100

    Update + Format

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
-# From https://github.com/litchipi/nix-build-templates/blob/6e4961dc56a9bbfa3acf316d81861f5bd1ea37ca/rust/maturin.nix
-# See also https://discourse.nixos.org/t/pyo3-maturin-python-native-dependency-management-vs-nixpkgs/21739/2
 {
   # Build Pyo3 package
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -11,12 +9,13 @@
     };
     crane = {
       url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import inputs.nixpkgs {
           inherit system;
@@ -25,19 +24,17 @@
         lib = pkgs.lib;
 
         # Get a custom rust toolchain
-        customRustToolchain = pkgs.rust-bin.stable."1.70.0".default;
-        craneLib =
-          (inputs.crane.mkLib pkgs).overrideToolchain customRustToolchain;
+        customRustToolchain = pkgs.rust-bin.stable."1.92.0".default;
+        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain customRustToolchain;
 
-        projectName =
-          (craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; }).pname;
-        projectVersion = (craneLib.crateNameFromCargoToml {
-          cargoToml = ./Cargo.toml;
-        }).version;
+        projectName = (craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; }).pname;
+        projectVersion =
+          (craneLib.crateNameFromCargoToml {
+            cargoToml = ./Cargo.toml;
+          }).version;
 
-        pythonVersion = pkgs.python310;
-        wheelTail =
-          "cp310-cp310-manylinux_2_34_x86_64"; # Change if pythonVersion changes
+        pythonVersion = pkgs.python313;
+        wheelTail = "cp313-cp313-linux_x86_64"; # Change if pythonVersion changes
         wheelName = "${projectName}-${projectVersion}-${wheelTail}.whl";
 
         crateCfg = {
@@ -46,31 +43,37 @@
         };
 
         # Build the library, then re-use the target dir to generate the wheel file with maturin
-        crateWheel = (craneLib.buildPackage (crateCfg // {
-          pname = projectName;
-          version = projectVersion;
-          # cargoArtifacts = crateArtifacts;
-        })).overrideAttrs (old: {
-          nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.maturin ];
-          buildPhase = old.buildPhase + ''
-            maturin build --offline --target-dir ./target
-          '';
-          installPhase = old.installPhase + ''
-            cp target/wheels/${wheelName} $out/
-          '';
-        });
-      in rec {
+        crateWheel =
+          (craneLib.buildPackage (
+            crateCfg
+            // {
+              pname = projectName;
+              version = projectVersion;
+              # cargoArtifacts = crateArtifacts;
+            }
+          )).overrideAttrs
+            (old: {
+              nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.maturin ];
+              buildPhase = old.buildPhase + ''
+                maturin build --offline --target-dir ./target
+              '';
+              installPhase = old.installPhase + ''
+                cp target/wheels/${wheelName} $out/
+              '';
+            });
+      in
+      rec {
         packages = rec {
           default = crateWheel; # The wheel itself
 
           # A python version with the library installed
-          pythonEnv = pythonVersion.withPackages
-            (ps: [ (lib.pythonPackage ps) ] ++ (with ps; [ ipython ]));
+          pythonEnv = pythonVersion.withPackages (ps: [ (lib.pythonPackage ps) ] ++ (with ps; [ ipython ]));
         };
 
         lib = {
           # To use in other builds with the "withPackages" call
-          pythonPackage = ps:
+          pythonPackage =
+            ps:
             ps.buildPythonPackage rec {
               pname = projectName;
               format = "wheel";
@@ -85,14 +88,21 @@
           rust = pkgs.mkShell {
             name = "rust-env";
             src = ./.;
-            nativeBuildInputs = with pkgs; [ pkg-config rust-analyzer maturin ];
+            nativeBuildInputs = with pkgs; [
+              customRustToolchain
+              pkg-config
+              rust-analyzer
+              maturin
+            ];
           };
-          python = let
-          in pkgs.mkShell {
-            name = "python-env";
-            src = ./.;
-            nativeBuildInputs = [ packages.pythonEnv ];
-          };
+          python =
+            let
+            in
+            pkgs.mkShell {
+              name = "python-env";
+              src = ./.;
+              nativeBuildInputs = [ packages.pythonEnv ];
+            };
           default = rust;
         };
 
@@ -103,5 +113,6 @@
           };
           default = ipython;
         };
-      });
+      }
+    );
 }
